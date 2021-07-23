c199821d9bb5ac2cdf5eaa3b5882c9109402ec46
zRCP <53063657+zRCP@users.noreply.github.com>
2021-07-23 11:11:46 -0400

    db

--- src/main/java/conexão/Conexao.java
+++ src/main/java/conexão/Conexao.java
@@ -10,6 +10,8 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.JOptionPane;
 
 /**
@@ -17,39 +19,46 @@ import javax.swing.JOptionPane;
  * @author rafae
  */
 public class Conexao {
-    public Statement stmt;
-    public ResultSet rs;
+    public static Connection conexao = null;
     
-    public Connection conexao;
-    
-    public Connection getConection() {
-  
+    public static Connection getConection() {
         try {
             conexao = DriverManager.getConnection
                         ("jdbc:postgresql://localhost:5432/EasySolar","postgres", "rc12345");
             System.out.println("Conectado com Sucesso");
         } catch (SQLException ex) {
-            
-            throw new RuntimeException(ex);
+            throw new DbException(ex.getMessage());
         }
         return conexao;
     }
     
-    public void closeCon(){
-        try {
-            conexao.close();
-        } catch (SQLException ex) {
-            JOptionPane.showMessageDialog(null, "Erro ao fechar conexao: "+ ex);
+    public static void closeConexao(){
+        if(conexao != null){
+            try {
+                conexao.close();
+            } catch (SQLException ex) {
+                throw new DbException(ex.getMessage());
+            }
         }
     }
-    public void executaSQL(String sql){
     
-        try {
-            stmt = conexao.createStatement(rs.TYPE_SCROLL_INSENSITIVE, rs.CONCUR_READ_ONLY);
-            rs = stmt.executeQuery(sql);
-            rs.next();
-        } catch (SQLException ex) {
-            throw new RuntimeException(ex);
+    public static void closeStatement(Statement stmt){
+        if(stmt != null){
+            try {
+                stmt.close();
+            } catch (SQLException ex) {
+              throw new DbException(ex.getMessage());
+            }
+        }
+    }
+    
+   public static void closeResult(ResultSet rs){
+        if(rs != null){
+            try {
+                rs.close();
+            } catch (SQLException ex) {
+              throw new DbException(ex.getMessage());
+            }
         }
     }
 }
